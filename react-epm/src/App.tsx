468bf00da6e6839d392897461e39c3e542460884
Alessa <alessa.mcleese@gmail.com>
2026-03-23 17:52:17 -0400

    feat: add shadcn components

---
+++ react-epm/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/app.scss'; // We will put global styles here
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Eos Planetary Monitor (React)</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-epm/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import cesium from 'vite-plugin-cesium';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  assetsInclude: ['**/*.glb'],
+  plugins: [react(), tailwindcss(), cesium()],
+  server: {
+    fs: {
+      // Allow serving files from one level up (the root)
+      allow: ['..'],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // This tells Sass to ignore the deprecation warning for @import
+        silenceDeprecations: ['import'],
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, '../shared-assets'),
+    },
+  },
+});
